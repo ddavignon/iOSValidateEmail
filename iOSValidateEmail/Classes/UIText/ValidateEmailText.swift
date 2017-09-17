594b62ae8b21bcc8a51c87750299ee199c0e4851
Dustin D'Avignon <ddavignon@csumb.edu>
2017-09-17 12:38:32 -0700

    initial commit

---
+++ iOSValidateEmail/Classes/UIText/ValidateEmailText.swift
@@ -0,0 +1,35 @@
+//
+//  ValidateEmailText.swift
+//  Pods
+//
+//  Created by Dustin D'Avignon on 9/17/17.
+//
+//
+
+import Foundation
+import UIKit
+
+public class ValidateEmailText: UITextField {
+    // validate email text, not reinventing the wheel: http://multithreaded.stitchfix.com/blog/2016/11/02/email-validation-swift/
+    public func validateEmail(field: UITextField) -> String? {
+        guard let trimmedText = field.text?.trimmingCharacters(in: .whitespacesAndNewlines) else {
+            return nil
+        }
+        
+        guard let dataDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) else {
+            return nil
+        }
+        
+        let range = NSMakeRange(0, NSString(string: trimmedText).length)
+        let allMatches = dataDetector.matches(in: trimmedText,
+                                              options: [],
+                                              range: range)
+        
+        if allMatches.count == 1,
+            allMatches.first?.url?.absoluteString.contains("mailto:") == true
+        {
+            return trimmedText
+        }
+        return nil
+    }
+}
